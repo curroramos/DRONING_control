84d91c10ce7b9ed2dbf5a7690be5fe1272ceb0c0
Francisco Ramos <curro@Franciscos-MacBook-Air.local>
2023-03-12 20:55:15 +0100

    1.2 version

--- control_utils.py
+++ control_utils.py
@@ -1,23 +1,6 @@
 import cv2 
 import numpy as np
 
-def movement_control(drone, dir):
-    if dir == -1:
-        drone.move_forward(100)
-    elif dir == 1:
-        drone.yaw_velocity = -60
-    elif dir == 2:
-        drone.yaw_velocity = 60
-    elif dir == 3:
-        drone.up_down_velocity= 60
-    elif dir == 4:
-        drone.up_down_velocity= -60
-    else:
-        drone.left_right_velocity = 0; drone.for_back_velocity = 0;drone.up_down_velocity = 0; drone.yaw_velocity = 0
-        
-    # SEND VELOCITY VALUES TO TELLO
-    if drone.send_rc_control:
-        drone.send_rc_control(drone.left_right_velocity, drone.for_back_velocity, drone.up_down_velocity, drone.yaw_velocity)
 
 
 def direction_control(cx,cy,object_detected, frameWidth, frameHeight, deadZone, imgContour):
@@ -48,3 +31,76 @@ def direction_control(cx,cy,object_detected, frameWidth, frameHeight, deadZone, 
         dir = 0
 
     return dir
+
+def movement_control(drone, dir, contador_securidad, num_puertas):
+    if contador_seguridad < 0:
+        contador_seguridad = 0
+    elif contador_seguridad > 11:
+        contador_seguridad = 10
+    
+    if dir == -1:
+        contador_seguridad +=1
+        print("Contador seguridad: ", contador_seguridad)
+        if contador_seguridad == 10:
+            contador_seguridad = 0
+            print("Atravesando puerta ...")
+            num_puertas += 1 
+            drone.left_right_velocity = 0; drone.for_back_velocity = 0;drone.up_down_velocity = 0; drone.yaw_velocity = 0
+            drone.move_forward(100)
+        else:
+            drone.left_right_velocity = 0; drone.for_back_velocity = 0;drone.up_down_velocity = 0; drone.yaw_velocity = 0
+
+    elif dir == 1:
+        contador_seguridad -=1
+        drone.yaw_velocity = -60
+    elif dir == 2:
+        contador_seguridad -=1
+        drone.yaw_velocity = 60
+    elif dir == 3:
+        contador_seguridad -=1
+        drone.up_down_velocity= 60
+    elif dir == 4:
+        contador_seguridad -=1
+        drone.up_down_velocity= -60
+    else:
+        contador_seguridad -=1
+        drone.left_right_velocity = 0; drone.for_back_velocity = 0;drone.up_down_velocity = 0; drone.yaw_velocity = 0
+
+    # SEND VELOCITY VALUES TO TELLO
+    if drone.send_rc_control:
+        drone.send_rc_control(drone.left_right_velocity, drone.for_back_velocity, drone.up_down_velocity, drone.yaw_velocity)
+
+    return contador_seguridad, num_puertas
+
+def movement_control_sim(dir, contador_seguridad, num_puertas):
+    if contador_seguridad < 0:
+        contador_seguridad = 0
+    elif contador_seguridad > 11:
+        contador_seguridad = 10
+
+    if dir == -1:
+        contador_seguridad +=1
+        print("Contador seguridad: ", contador_seguridad)
+        if contador_seguridad > 10:
+            contador_seguridad = 0
+            print("Atravesando puerta ...")
+            num_puertas+=1
+        else:
+            print("Dron quieto")
+    elif dir == 1:
+        contador_seguridad -=1
+        print("Girando izquierda")
+    elif dir == 2:
+        contador_seguridad -=1
+        print("Girando derecha")
+    elif dir == 3:
+        contador_seguridad -=1
+        print("Subiendo")
+    elif dir == 4:
+        contador_seguridad -=1
+        print("Bajando")
+    else:
+        contador_seguridad -=1
+        print("Dron quieto")
+    
+    return contador_seguridad, num_puertas
--- main.py
+++ main.py
@@ -7,17 +7,24 @@ from control_utils import *
 ##################
 using_tello = 0 # poner a 1 si se tiene el dron
 tello_cam = 0 # poner a 1 si se tiene el dron y sólo se quiere usar la cámara
+detector = 0 # 0: color, 1: mano
+num_total_puertas = 1 # número total de puertas en el circuito  
 ##################
 
-# Image configuration
+# Configuración de imagen
 width = 640 
 height = 480  
 deadZone =100
 frameWidth = width
 frameHeight = height
 
+
 # Variable global para la imagen
 global imgContour
+
+# Contadores para control de dron 
+cnt_sec = 0
+num_puertas = 0
 
 # Conectar dron
 if using_tello or tello_cam:
@@ -39,9 +46,9 @@ else:
     drone.streamoff()
     drone.streamon()
 
-
 # Crear ventana con trackbars
-create_trackbar(empty)
+if detector == 0:
+    create_trackbar(empty)
 
 # Bucle principal
 while True:
@@ -56,13 +63,18 @@ while True:
 
     imgContour = img.copy()
     
-    # Usar trackbars y filtros para preprocesamiento de imagen
-    result, imgDil = procesamiento_imagen(img)
+    # Uso de detector
+    if detector == 0:
+        # Usar trackbars y filtros para preprocesamiento de imagen
+        result, imgDil = procesamiento_imagen(img)
 
-    # Detección de contornos
-    cx,cy,object_detected = getContours(imgDil, imgContour, frameWidth, frameHeight, deadZone)
-    display(imgContour, frameWidth, frameHeight, deadZone)
-    print(cx, cy, object_detected)
+        # Detección de contornos
+        cx,cy,object_detected = getContours(imgDil, imgContour, frameWidth, frameHeight, deadZone)
+        display(imgContour, frameWidth, frameHeight, deadZone)
+        print(cx, cy, object_detected)
+
+    elif detector ==1:
+        pass
 
     # Control de dirección
     dir = direction_control(cx,cy,object_detected, frameWidth, frameHeight, deadZone, imgContour)
@@ -75,15 +87,17 @@ while True:
             drone.takeoff()
             startCounter = 1
         # Actuar según dirección del objeto detectado
-        movement_control(drone, dir)
+        cnt_sec, num_puertas = movement_control(drone, dir, cnt_sec, num_puertas)
+    else:
+        cnt_sec, num_puertas = movement_control_sim(dir,cnt_sec, num_puertas)
 
     # Display 
     stack = stackImages(0.9, ([img, result], [imgDil, imgContour]))
     cv2.imshow('Horizontal Stacking', stack)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q') or num_puertas==num_total_puertas:
         if using_tello:
-            # Aterrizaje de emergencia
+            # Aterrizaje y fin de misión
             drone.land()
         break
 
